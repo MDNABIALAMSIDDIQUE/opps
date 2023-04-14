8b91dad8bc2ab1b909d30366acd765a2ee07e7c7
MDNABIALAMSIDDIQUE <nabijavamd@gmail.com>
2023-04-15 01:00:57 +0530

    boot

--- SpringBoot/SpringBoot/src/main/java/com/springboot/Application.java
+++ SpringBoot/SpringBoot/src/main/java/com/springboot/Application.java
@@ -1,13 +1,33 @@
 package com.springboot;
 
+import java.time.LocalDateTime;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import com.springboot.sbean.WishMessage;
 
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+    @Bean(name = "local")
+    LocalDateTime ldt() {
+		System.out.println("Local Date time Predifne Class");
+		return LocalDateTime.now();
+		
 	}
+
+	public static void main(String[] args) {
+	ApplicationContext ctx=	SpringApplication.run(Application.class, args);
+		
+		WishMessage genretor=ctx.getBean("wish",WishMessage.class);
+		String result=genretor.showMessage();
+		System.out.println(result+":   MD NABI ALAM");
+		
+		
+		
+				}
 
 }
